cc7baa2eb085caedd9bd1f508b57d3c2e0f8d011
Listerine87 <brettjmill@gmail.com>
2021-06-09 17:33:29 -0700

    First commit of BoxingUnboxing assignment

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> boxList = new List<object>();
+
+            boxList.Add(7);
+            boxList.Add(28);
+            boxList.Add(-1);
+            boxList.Add(true);
+            boxList.Add("chair");
+
+            for (int i = 0; i < boxList.Count; i++)
+            {
+                Console.WriteLine(boxList[i]);
+            }
+
+            var total = 0;
+            for (int i = 0; i < boxList.Count; i++)
+            {
+                if (boxList[i] is int)
+                {
+                    int tempNum = (int)boxList[i];
+                    total += tempNum;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
